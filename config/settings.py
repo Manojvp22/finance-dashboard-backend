c7d90e02d5eb88f0f9f8f121dd464d521d6f551a
Manoj V Poojar <manojvpoojar@gmail.com>
2026-04-05 15:05:49 +0530

    fix csrf trusted origins for railway

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://finance-dashboard-backend-production-3173.up.railway.app"
+]
